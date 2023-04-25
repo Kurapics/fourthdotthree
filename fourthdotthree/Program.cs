4e9dcbd455a2533da64178af72c237c87b80909b
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:26:23 +0200

    Добавьте файлы проекта.

---
+++ fourthdotthree/Program.cs
@@ -0,0 +1,7 @@
+﻿double a = 1.35;
+double b = -6.25;
+
+for (double i = 5.3; i == 10.3; i += 0.25)
+{
+    Console.WriteLine("y = " + (Math.Pow((a * i), 3) + Math.Pow(Math.Cos(Math.Pow(i, 3) - b), 2)));
+}
